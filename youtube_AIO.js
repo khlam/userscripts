973c37797877444ced79b71ecd0aff77944d224e
khlam <4841220+khlam@users.noreply.github.com>
2022-03-12 13:45:56 -0800

    youtube all / links to subscriptions page

--- youtube_AIO.js
+++ youtube_AIO.js
@@ -9,21 +9,23 @@
 // ==/UserScript==
 
 // fix link auto-redirect. source: https://greasyfork.org/en/scripts/4298-youtube-url-cleaner/code
-if (window.location.href !== "https://www.youtube.com/feed/subscriptions"){
+if (window.location.href !== "https://www.youtube.com/feed/subscriptions") {
     var newurl = "https://www.youtube.com/watch?"+document.URL.match(/v\=[^&]*/g)
     if (newurl != document.URL) location.replace(newurl);
-    
-    // auto redirect youtube homepage to subscriptions
-    function redirect_to_subs() {
+}
+
+// auto redirect youtube homepage to subscriptions
+function redirect_to_subs() {
+    if (window.location.href !== "https://www.youtube.com/feed/subscriptions") {
         if (window.location.href === "https://www.youtube.com/" || window.location.href === "http://www.youtube.com/" ) {
             window.location.href = "https://www.youtube.com/feed/subscriptions"
         }
     }
-    
-    window.addEventListener("yt-navigate-finish", redirect_to_subs)
-    
-    redirect_to_subs()
 }
+
+window.addEventListener("yt-navigate-finish", redirect_to_subs)
+
+redirect_to_subs()
 
 const observer = new MutationObserver(function() {
     try {
@@ -31,6 +33,14 @@ const observer = new MutationObserver(function() {
         console.log(dialog[0])
         dialog[0].style.display = 'none'
     }catch(e){console.log("err", e)}
+
+    let home_links = document.getElementsByTagName('a')
+    home_links.forEach(_link => {
+        if (_link.href === "/") {
+            console.log(_link)
+            _link.href = "https://www.youtube.com/feed/subscriptions"
+        }
+    })
 })
 
 observer.observe(document, {childList: true, characterData: true, subtree: true});
