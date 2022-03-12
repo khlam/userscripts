95bffb225b59cfa145fa7718c4e361c64797268d
khlam <4841220+khlam@users.noreply.github.com>
2022-03-12 13:15:38 -0800

    mutation observer to hide card

--- youtube_AIO.js
+++ youtube_AIO.js
@@ -4,32 +4,33 @@
 // @version		0.0
 // @match       http://*.youtube.com/*
 // @match       https://*.youtube.com/*
-// @exclude     https://www.youtube.com/feed/subscriptions
 // @grant		none
 // @license     MIT
 // ==/UserScript==
 
 // fix link auto-redirect. source: https://greasyfork.org/en/scripts/4298-youtube-url-cleaner/code
-var newurl = "https://www.youtube.com/watch?"+document.URL.match(/v\=[^&]*/g); 
-if (newurl != document.URL) location.replace(newurl);
-
-// removes youtube tv ad card
-try {
-    let dialog = document.getElementsByTagName("ytd-mealbar-promo-renderer")
-    if (dialog) {
-        dialog.forEach(e => {
-            thread.style.display = "none"
-        })
+if (window.location.href !== "https://www.youtube.com/feed/subscriptions"){
+    var newurl = "https://www.youtube.com/watch?"+document.URL.match(/v\=[^&]*/g)
+    if (newurl != document.URL) location.replace(newurl);
+    
+    // auto redirect youtube homepage to subscriptions
+    function redirect_to_subs() {
+        if (window.location.href === "https://www.youtube.com/" || window.location.href === "http://www.youtube.com/" ) {
+            window.location.href = "https://www.youtube.com/feed/subscriptions"
+        }
     }
-}catch(e){}
-
-// auto redirect youtube homepage to subscriptions
-function redirect_to_subs() {
-    if (window.location.href === "https://www.youtube.com/" || window.location.href === "http://www.youtube.com/" ) {
-        window.location.href = "https://www.youtube.com/feed/subscriptions"
-    }
+    
+    window.addEventListener("yt-navigate-finish", redirect_to_subs)
+    
+    redirect_to_subs()
 }
 
-window.addEventListener("yt-navigate-finish", redirect_to_subs)
+const observer = new MutationObserver(function() {
+    try {
+        let dialog = document.getElementsByTagName("ytd-mealbar-promo-renderer")
+        console.log(dialog[0])
+        dialog[0].style.display = 'none'
+    }catch(e){console.log("err", e)}
+})
 
-redirect_to_subs()
+observer.observe(document, {childList: true, characterData: true, subtree: true});
