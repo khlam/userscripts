574826a524a4d2123f354e40b8acbcb597fc9584
khlam <4841220+khlam@users.noreply.github.com>
2022-03-14 11:45:10 -0700

    update twitch front carousel block

--- twitch_front_carousel_block.js
+++ twitch_front_carousel_block.js
@@ -1,31 +1,38 @@
 // ==UserScript==
 // @name         twitch_front_carousel_block
 // @version      0.0
-// @description  Blocks twitch frontpage autoplaying stream by setting its source to null.
+// @description  Blocks twitch frontpage auto-playing stream by setting its source to null. Refresh page if stream you want to play has an error.
 // @author       khlam
-// @match        http://www.twitch.tv/
-// @match        https://www.twitch.tv/
+// @match        http://*.twitch.tv/*
+// @match        https://*.twitch.tv/*
 // @grant        none
-// @run-at       document-end
-// @license     MIT
+// @license      MIT
 // ==/UserScript==
 
-if (window.location.href === "https://www.twitch.tv/" || window.location.href === "http://www.twitch.tv/" ) {
-    let i = 0
-    let interval = setInterval(() => {
-        let video = document.querySelector("video")
-        console.log(i)
-        if (video.getAttribute('src') != null) {
-            console.log("setting video src to null...")
-            video.setAttribute('src', null)
-            clearInterval(interval)
+const observer = new MutationObserver(function() {
+    try {
+        if (window.location.href === "https://www.twitch.tv/" || window.location.href === "http://www.twitch.tv/" ) {
+        let i = 0
+        let interval = setInterval(() => {
+            let video = document.querySelector("video")
+            console.log(i)
+            if (video.getAttribute('src') != null) {
+                console.log("setting video src to null...")
+                video.setAttribute('src', null)
+                clearInterval(interval)
+            }
+            i += 1
+            if (i === 100) {
+                clearInterval(interval)
+            }
+            }, 100)
         }
-        i += 1
-        if (i === 100) {
-            clearInterval(interval)
-        }
-    }, 100)
-}
+    }catch(e){console.log("err", e)}
 
-// remove twitch begging for you to sign in
-document.getElementById("twilight-sticky-footer-root").style.display = "none"
+    try {
+        // remove twitch begging for you to sign in
+        document.getElementById("twilight-sticky-footer-root").style.display = "none"
+    }catch(e){console.log("err", e)}
+})
+
+observer.observe(document, {childList: true, characterData: true, subtree: true})
